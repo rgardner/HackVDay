0ead567f4b19065e94b70b773b6cd9c0b19a7d15
Robert Gardner <rhg259@nyu.edu>
2014-02-08 21:29:34 -0500

    Add delivery form

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -2,5 +2,22 @@
 
 angular.module('hackvdayApp')
   .controller('MainCtrl', function ($scope, $firebase) {
-    $scope.messages = $firebase(new Firebase('https://brilliant-fire-2550.firebaseio.com/chat'));
+    $scope.deliveries = $firebase(new Firebase('https://brilliant-fire-2550.firebaseio.com/deliveries'));
+    $scope.delivery = {};
+    $scope.dorms = [
+        'Carlyle Court', 'Founders Hall', 'Goddard Hall', 'Weinstein',
+        'Third North'
+      ];
+    $scope.addDelivery = function() {
+        $scope.deliveries.$add({recipient: $scope.delivery.recipient,
+                                message:  $scope.delivery.message,
+                                sender:     $scope.delivery.sender,
+                                phoneNumber: $scope.delivery.phoneNumber,
+                                rose:     $scope.delivery.rose,
+                                chocolate: $scope.delivery.chocolate,
+                                destination: $scope.delivery.destination,
+                                roomNumber: $scope.delivery.roomNumber
+                        });
+        $scope.newPerson = {};
+    }
   });
