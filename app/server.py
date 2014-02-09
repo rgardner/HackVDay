38368839e4f7bd708dc55410533141f2c38399d4
Abhi Agarwal <me@abhiagarwal.com>
2014-02-09 02:16:25 -0500

    Update

---
+++ app/server.py
@@ -0,0 +1,38 @@
+import tornado.escape
+import tornado.ioloop
+import tornado.options
+import tornado.web
+import tornado.websocket
+import os.path
+from tornado.options import define, options
+
+define("port", default = 8000, help = "port to run on", type = int)
+
+class Application(tornado.web.Application):
+    def __init__(self):
+        handlers = [
+            (r"/", MainHandler),
+        ]
+        settings = dict(
+            cookie_secret = "Dk3Pk/kFRHCukMLUYyfo3p7wXzhDiU+3rYx2K0/8po4",
+            template_path = os.path.join(os.path.dirname(__file__), ""),
+            static_path = os.path.join(os.path.dirname(__file__), "static"),
+            xsrf_cookies = True,
+            debug = True,
+        )
+        tornado.web.Application.__init__(self, handlers, **settings)
+
+class MainHandler(tornado.web.RequestHandler):
+    def get(self):
+        self.render("index.html")
+
+def main():
+    tornado.options.parse_command_line()
+    app = Application()
+    app.listen(options.port)
+    tornado.autoreload.add_reload_hook(main)
+    tornado.autoreload.start()
+    tornado.ioloop.IOLoop.instance().start()
+
+if __name__ == "__main__":
+    main()
