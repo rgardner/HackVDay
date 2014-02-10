3bce58a6fd071e7030cd1bb4a1590dcda14851ea
Robert Gardner <rhg259@nyu.edu>
2014-02-09 21:42:08 -0500

    Add min styled thank-you text

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -12,6 +12,8 @@ angular.module('hackvdayApp')
         'Rubin Hall', 'Second Street', 'Seventh Street', 'Third Avenue North',
         '13th Street', '26th Street', 'University Hall', 'Weinstein Hall'
       ];
+    $scope.formSuccess = false;
+    $scope.previousRecipient = '';
     $scope.addDelivery = function() {
         $scope.deliveries.$add({recipient: $scope.delivery.recipient,
                                 message:  $scope.delivery.message,
@@ -20,8 +22,13 @@ angular.module('hackvdayApp')
                                 dorm: $scope.delivery.dorm,
                                 roomNumber: $scope.delivery.roomNumber
                         });
+        $scope.previousRecipient = $scope.delivery.recipient;
         $scope.delivery = {};
         $scope.deliveryForm.$setPristine();
+        $scope.formSuccess = true;
+
+        /* hacky, can't get thanks text to show without it. */
+        console.log($scope.formSuccess);
 
         $location.hash('top');
         $anchorScroll();
